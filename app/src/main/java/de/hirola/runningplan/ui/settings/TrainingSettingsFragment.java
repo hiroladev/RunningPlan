330bb0bada82900e4a081dfa00041b3a91c36c8e
mis <micha.schmidt@secure.mailbox.org>
2021-11-23 14:31:50 +0100

    Init

---
+++ app/src/main/java/de/hirola/runningplan/ui/settings/TrainingSettingsFragment.java
@@ -0,0 +1,13 @@
+package de.hirola.runningplan.ui.settings;
+
+import de.hirola.runningplan.R;
+import android.os.Bundle;
+import androidx.preference.PreferenceFragmentCompat;
+
+public class TrainingSettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.training_settings_preferences, rootKey);
+    }
+}
